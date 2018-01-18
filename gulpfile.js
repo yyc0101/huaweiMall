7f32459b353c80c233f9e24c8a6bc282d4cdf8e1
cyy0101 <350628442@qq.com>
2018-01-18 22:43:13 +0800

    添加了轮播图

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+//引用gulp导入工具包（node-modules里对应的模块）
+var gulp = require('gulp');
+//sass插件文件
+var sass = require('gulp-sass');
+
+//合并插件
+/* var concat = require('gulp-concat');
+//引入gulp-uglify压缩插件
+var uglify = require("gulp-uglify");
+//引入gulp-rename重命名插件
+var rename = require("gulp-rename"); */
+
+gulp.task("copy-index",function () {
+    //复制文件
+    //gulp.src("index.html").pipe(gulp.dest("dist"));
+    gulp.src("*.html").pipe(gulp.dest("D:\\phpStudy\\WWW\\html"));
+});
+
+gulp.task('copy-scss', function(){
+    gulp.src('sass/*.scss') 
+    .pipe(sass())
+    .pipe(gulp.dest('D:\\phpStudy\\WWW\\css'));
+});
+
+gulp.task('copy-jpg', function(){
+    gulp.src('imgs/*.*')
+    .pipe(gulp.dest('D:\\phpStudy\\WWW\\imgs'));
+});
+
+gulp.task('copy-js', function(){
+    gulp.src('js/*.js')
+    .pipe(gulp.dest('D:\\phpStudy\\WWW\\js'));
+});  
+
+//合并压缩重命名文件
+/* gulp.task('concatuglify', function(){
+    gulp.src(['js/index.js', 'js/goods.js'])
+        .pipe(concat('concat.js'))         //将上面两个文件合并成一个
+        .pipe(gulp.dest('js'))            //合并后要发送的地址
+        .pipe(uglify())                  //压缩
+        .pipe(rename("concat.min.js"))   //压缩后重命名
+        .pipe(gulp.dest('js'));         //压缩后要发布的地址        
+}) */
+
+gulp.task('watch', function(){
+    gulp.watch('*.html', ['copy-index']);
+    gulp.watch('sass/*.scss', ['copy-scss']);
+   gulp.watch('imgs/*.jpg', ['copy-jpg']);
+    gulp.watch('js/*.js', ['copy-js']); 
+    // gulp.watch(['js/index.js','js/goods.js'],['concatuglify']);
+})
---
+++ js/index.js
@@ -0,0 +1,138 @@
+//广告消失首部
+function ad(){
+    $('#xx').mouseenter(function(){
+        $(this).css({'cursor':'pointer'})
+    });
+    $('#xx').click(function(){
+        $('#bgimgs').css({'display':'none'})
+    });
+}
+//更多
+function more(){
+    $('#show').mouseenter(function(){
+        $('#down-more').css({
+            display:'block',
+        });
+    });
+    //更多
+    $('#show').mouseleave(function(){
+        $('#down-more').css({
+            display:'none',
+           background:'#fff',
+        });
+    });
+    //购物车
+    $('#regist  li:last-child').mouseenter(function(){
+        $('#shoppCart').css({
+            background:'#fff',
+            display:'block',
+        });
+    });
+    $('#regist  li:last-child').mouseleave(function(){
+        $('#shoppCart').css({
+            display:'none',
+        });
+    });
+    //logoye搜索框
+    $('.onsbm form input').focus(function(){
+        $('.right li:nth-child(1)').css({
+            display:'none',
+        })
+        $('.right li:nth-child(2)').css({
+            display:'none',
+        })
+        $('.right li:nth-child(3)').css({
+
+        })
+    })
+}
+
+//轮播图
+var arr=["bo1.jpg","bo2.jpg.jpg","bo3.jpg.jpg","bo4.jpg.jpg","bo5.jpg.jpg","bo6.jpg.jpg","bo7.jpg.jpg"];
+var ord = 0;//代表当前图片的序号，从0开始。
+var myTimer = null;
+
+function  initUI() {
+    $("#lunbo li:first").css({"backgroundColor":"white"});
+}
+
+function  initEvent() {
+    $("#lunbo").mouseenter(function () {
+        stopPlay();
+    });
+
+    $("#lunbo").mouseleave(function () {
+        autoPlay();
+    });
+
+    $("#lunbo li").mouseenter(function () {
+        goImg($("#lunbo li").index(this));
+    });
+}
+
+//1、自动播放
+function autoPlay() {
+    myTimer = setInterval(function () {
+        //一、改变数据
+        // 1、记录当前序号（淡出的图片序号）
+        let outOrd = ord;
+        //2、改变要显示的图片的序号（淡出图片序号加一）
+        ord++;
+        if(ord>arr.length-1){
+            ord=0;
+        }
+        //二、改变外观
+        let $img = $("#lunbo img");
+        //1、淡入淡出效果
+        //让一张(outOrd)淡出 当前消失
+        $img.eq(outOrd).animate({"opacity":0},2000);
+        //让一张(ord)淡入下一张图片显示
+        $img.eq(ord).animate({"opacity":1},2000);
+        //2、改变豆豆的颜色；
+        $("#lunbo li").eq(ord).css({"backgroundColor":"white"}).siblings().css({"backgroundColor":""});
+    },3000);
+}
+
+//2、停止播放
+function stopPlay() {
+    window.clearInterval(myTimer);
+}
+
+//3、跳转到指定的图片
+function  goImg(transOrd) {
+    //一、改变数据
+    // 1、记录当前序号（淡出的图片序号）
+    let outOrd = ord;
+    //2、改变要显示的图片的序号（传入的图片序号）
+    ord=transOrd;
+    if(ord>arr.length-1){
+        ord=0;
+    }
+    //二、改变外观
+    let $img = $("#lunbo img");
+    //1、淡入淡出效果
+    //让一张(outOrd)淡出 当前消失
+    $img.eq(outOrd).animate({"opacity":0},500);
+    //让一张(ord)淡入下一张图片显示
+    $img.eq(ord).animate({"opacity":1},500);
+    //2、改变豆豆的颜色；
+    $("#lunbo li").eq(ord).css({"backgroundColor":"white"}).siblings().css({"backgroundColor":""});
+}
+
+
+
+
+
+
+
+$(function(){
+    ad();
+    more();
+    //轮播图
+    //1、初始化界面
+    initUI();
+    //2、绑定事件
+    initEvent();
+    //3、自动播放
+    autoPlay();
+})
