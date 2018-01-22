ce2b81d7dbb6e89d231d80a239f3d3c07cca0c29
cyy0101 <350628442@qq.com>
2018-01-22 08:37:29 +0800

    add register.html

--- js/index.js
+++ js/index.js
@@ -100,36 +100,64 @@ function more(){
 
 more();
 //轮播图
-var arr=["bo1.jpg","bo2.jpg.jpg","bo3.jpg.jpg","bo4.jpg.jpg","bo5.jpg.jpg","bo6.jpg.jpg","bo7.jpg.jpg"];
-var ord = 0;//代表当前图片的序号，从0开始。
-var myTimer = null;
-
-function  initUI() {
-    $("#lunbo li:first").css({"backgroundColor":"white"});
-}
-
-function  initEvent() {
-    $("#lunbo").mouseenter(function () {
-        stopPlay();
-    });
-
-    $("#lunbo").mouseleave(function () {
-        autoPlay();
-    });
-
-    $("#lunbo li").mouseenter(function () {
-        goImg($("#lunbo li").index(this));
-    });
-}
-
-//1、自动播放
-function autoPlay() {
-    myTimer = setInterval(function () {
+function lun1(){
+    var arr=["z1.jpg","z2.jpg.jpg","z3.jpg.jpg","z4.jpg.jpg","z5.jpg.jpg","z6.jpg.jpg","z7.jpg.jpg"];
+    var ord = 0;//代表当前图片的序号，从0开始。
+    var myTimer = null;
+    
+    function  initUI() {
+        $("#lunbo li:first").css({"backgroundColor":"white"});
+    }
+    
+    function  initEvent() {
+        $("#lunbo").mouseenter(function () {
+            stopPlay();
+        });
+    
+        $("#lunbo").mouseleave(function () {
+            autoPlay();
+        });
+    
+        $("#lunbo li").mouseenter(function () {
+            goImg($("#lunbo li").index(this));
+        });
+    }
+    
+    //1、自动播放
+    function autoPlay() {
+        myTimer = setInterval(function () {
+            //一、改变数据
+            // 1、记录当前序号（淡出的图片序号）
+            let outOrd = ord;
+            //2、改变要显示的图片的序号（淡出图片序号加一）
+            ord++;
+            if(ord>arr.length-1){
+                ord=0;
+            }
+            //二、改变外观
+            let $img = $("#lunbo img");
+            //1、淡入淡出效果
+            //让一张(outOrd)淡出 当前消失
+            $img.eq(outOrd).animate({"opacity":0},2000);
+            //让一张(ord)淡入下一张图片显示
+            $img.eq(ord).animate({"opacity":1},2000);
+            //2、改变豆豆的颜色；
+            $("#lunbo li").eq(ord).css({"backgroundColor":"white"}).siblings().css({"backgroundColor":""});
+        },2000);
+    }
+    
+    //2、停止播放
+    function stopPlay() {
+        window.clearInterval(myTimer);
+    }
+    
+    //3、跳转到指定的图片
+    function  goImg(transOrd) {
         //一、改变数据
         // 1、记录当前序号（淡出的图片序号）
         let outOrd = ord;
-        //2、改变要显示的图片的序号（淡出图片序号加一）
-        ord++;
+        //2、改变要显示的图片的序号（传入的图片序号）
+        ord=transOrd;
         if(ord>arr.length-1){
             ord=0;
         }
@@ -137,46 +165,104 @@ function autoPlay() {
         let $img = $("#lunbo img");
         //1、淡入淡出效果
         //让一张(outOrd)淡出 当前消失
-        $img.eq(outOrd).animate({"opacity":0},2000);
+        $img.eq(outOrd).animate({"opacity":0},500);
         //让一张(ord)淡入下一张图片显示
-        $img.eq(ord).animate({"opacity":1},2000);
+        $img.eq(ord).animate({"opacity":1},500);
         //2、改变豆豆的颜色；
         $("#lunbo li").eq(ord).css({"backgroundColor":"white"}).siblings().css({"backgroundColor":""});
-    },3000);
-}
-
-//2、停止播放
-function stopPlay() {
-    window.clearInterval(myTimer);
-}
-
-//3、跳转到指定的图片
-function  goImg(transOrd) {
-    //一、改变数据
-    // 1、记录当前序号（淡出的图片序号）
-    let outOrd = ord;
-    //2、改变要显示的图片的序号（传入的图片序号）
-    ord=transOrd;
-    if(ord>arr.length-1){
-        ord=0;
     }
-    //二、改变外观
-    let $img = $("#lunbo img");
-    //1、淡入淡出效果
-    //让一张(outOrd)淡出 当前消失
-    $img.eq(outOrd).animate({"opacity":0},500);
-    //让一张(ord)淡入下一张图片显示
-    $img.eq(ord).animate({"opacity":1},500);
-    //2、改变豆豆的颜色；
-    $("#lunbo li").eq(ord).css({"backgroundColor":"white"}).siblings().css({"backgroundColor":""});
-}
-
+    
+    //轮播图
+    //1、初始化界面
+    initUI();
+    //2、绑定事件
+    initEvent();
+    //3、自动播放
+    autoPlay();
+};
+lun1();
 //轮播图
-//1、初始化界面
-initUI();
-//2、绑定事件
-initEvent();
-//3、自动播放
-autoPlay();
+function lun2(){
+    var arr=["bo1.jpg","bo2.jpg.jpg","bo3.jpg.jpg","bo4.jpg.jpg","bo5.jpg.jpg","bo6.jpg.jpg","bo7.jpg.jpg"];
+    var ord = 0;//代表当前图片的序号，从0开始。
+    var myTimer = null;
+    
+    function  initUI() {
+        $("#lunbos li:first").css({"backgroundColor":"#ccc"});
+    }
+    
+    function  initEvent() {
+        $("#lunbos").mouseenter(function () {
+            stopPlay();
+        });
+    
+        $("#lunbos").mouseleave(function () {
+            autoPlay();
+        });
+    
+        $("#lunbos li").mouseenter(function () {
+            goImg($("#lunbos li").index(this));
+        });
+    }
+    
+    //1、自动播放
+    function autoPlay() {
+        myTimer = setInterval(function () {
+            //一、改变数据
+            // 1、记录当前序号（淡出的图片序号）
+            let outOrd = ord;
+            //2、改变要显示的图片的序号（淡出图片序号加一）
+            ord++;
+            if(ord>arr.length-1){
+                ord=0;
+            }
+            //二、改变外观
+            let $img = $("#lunbos img");
+            //1、淡入淡出效果
+            //让一张(outOrd)淡出 当前消失
+            $img.eq(outOrd).animate({"opacity":0},2000);
+            //让一张(ord)淡入下一张图片显示
+            $img.eq(ord).animate({"opacity":1},2000);
+            //2、改变豆豆的颜色；
+            $("#lunbos li").eq(ord).css({"backgroundColor":"#cb8177"}).siblings().css({"backgroundColor":"#ccc"});
+        },3000);
+    }
+    
+    //2、停止播放
+    function stopPlay() {
+        window.clearInterval(myTimer);
+    }
+    
+    //3、跳转到指定的图片
+    function  goImg(transOrd) {
+        //一、改变数据
+        // 1、记录当前序号（淡出的图片序号）
+        let outOrd = ord;
+        //2、改变要显示的图片的序号（传入的图片序号）
+        ord=transOrd;
+        if(ord>arr.length-1){
+            ord=0;
+        }
+        //二、改变外观
+        let $img = $("#lunbos img");
+        //1、淡入淡出效果
+        //让一张(outOrd)淡出 当前消失
+        $img.eq(outOrd).animate({"opacity":0},500);
+        //让一张(ord)淡入下一张图片显示
+        $img.eq(ord).animate({"opacity":1},500);
+        //2、改变豆豆的颜色；
+        $("#lunbos li").eq(ord).css({"backgroundColor":"#cb8177"}).siblings().css({"backgroundColor":"#ccc"});
+    }
+    
+    //轮播图
+    //1、初始化界面
+    initUI();
+    //2、绑定事件
+    initEvent();
+    //3、自动播放
+    autoPlay();
+};
+lun2();
 
 
+//轮播图（无缝滚动）
